aafda307ea82f7fcbfb4ee15211815af6692ad91
AnchalPandey18 <AnchalPandey1801@gmail.com>
2026-09-02 10:04:42 +0530

    implement authentication and jwt authorization

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+    registerUser,
+    loginUser
+} from "../controllers/authController.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+router.get("/me", authMiddleware, (req, res) => {
+    res.json({
+        message: "You are authenticated",
+        user: req.user
+    });
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import authRoutes from "./routes/authRoutes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
